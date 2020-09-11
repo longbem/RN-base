e3b59a31cdde36406eb08ccec22dbd63ac90d7a9
Hoang Long <longcomputer9x@gmail.com>
2020-09-11 14:25:35 +0700

    base

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'babel-plugin-transform-typescript-metadata',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-  ],
 };
--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { Text, SafeAreaView } from 'react-native';
 
 export default function AccountScreen() {
-  const a = 2;
-  const b = 0;
   return (
     <SafeAreaView>
       <Text>Account screen</Text>
