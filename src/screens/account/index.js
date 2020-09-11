98f26a769a8db72786d9b140697bad2ca24de2fb
Hoang Long <longcomputer9x@gmail.com>
2020-09-11 13:57:29 +0700

    base

--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -3,7 +3,7 @@ import { View, Text, SafeAreaView } from 'react-native';
 
 export default function AccountScreen() {
   const a = 2;
-
+  const b = 0;
   return (
     <SafeAreaView>
       <Text>Account screen</Text>
